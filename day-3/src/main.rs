4c698467b6f020b4cea48ff7a5b559532abad16a
Diogenes Risso <diogenesmarquesr@gmail.com>
2023-01-13 22:32:38 -0300

    day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -27,5 +27,26 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("part 1 result: {part1_result}");
 
+    // part 2
+    let lines = INPUT.lines()
+        .collect::<Vec<&str>>();
+
+    let chunks = lines
+        .chunks(3)
+        .collect::<Vec<&[&str]>>();
+
+    let part2_result: usize = chunks.iter()
+        .flat_map(|chunk| {
+            chunk[0].chars()
+                .find(|char| {
+                    (chunk[0].contains(*char) && chunk[1].contains(*char)) &&
+                    (chunk[2].contains(*char))
+                })
+        })
+        .map(|char| get_priority(char))
+        .sum::<usize>();
+
+    println!("part 2 result: {:?}", part2_result);
+
     Ok(())
 }
